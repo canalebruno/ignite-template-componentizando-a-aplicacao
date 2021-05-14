b74f8783655f2bfbe1286ce1bae30426a46a624a
Bruno Canale <b.canalemedeiros@gmail.com>
2021-05-14 08:48:56 -0300

    challange done

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,36 @@
-export function Content() {
+import { MovieCard } from './MovieCard';
+
+interface ContentProps {
+  movies: {
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }[];
+}
+
+export function Content(props: ContentProps) {
   // Complete aqui
+
+  const { movies } = props;
+
+  return (
+    <main>
+      <div className="movies-list">
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.imdbID}
+            title={movie.Title}
+            poster={movie.Poster}
+            runtime={movie.Runtime}
+            rating={movie.Ratings[0].Value}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
